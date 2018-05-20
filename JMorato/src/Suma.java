2d5154d66aacd4f5b81700ae32f8a2da4ae2b285
Moretti <blog@standardinet.com>
2018-05-20 03:31:58 -0700

    Version suma 1

---
+++ JMorato/src/Suma.java
@@ -0,0 +1,22 @@
+
+public class Suma {
+	
+	private int sumandoA;
+	private int sumandoB;
+	private int total;
+	
+	public Suma(int a, int b){
+		
+		sumandoA = a;
+		sumandoB = b;
+		
+		System.out.println("Total: " + suma(a,b));
+
+	}
+	
+	public int suma(int a, int b){
+		return a + b;
+		
+	}
+
+}
